98a0a2c7b15f12619952d7ce4b8f30f0dc290df5
Pedro Castro <F0lha@users.noreply.github.com>
2014-09-29 16:25:44 +0000

    lab1 completed

--- lab1/video_txt.c
+++ lab1/video_txt.c
@@ -41,28 +41,104 @@ void vt_blank() {
 }
 
 int vt_print_char(char ch, char attr, int r, int c) {
-  
-  /* To complete ... */
-
+  char *ptr;
+  if(r>scr_lines||c>scr_width) return 1;
+  ptr = video_mem;
+  ptr += scr_width*2*r;
+  ptr += 2*c;
+  ptr -= 2;
+  *ptr = ch;
+  ptr++;
+  *ptr = attr;
+  return 0;
 }
+  
+
+
 
 int vt_print_string(char *str, char attr, int r, int c) {
-
-  /* To complete ... */
-
+	  char *ptr,*ptr_ori;
+	  ptr = video_mem;
+	  ptr_ori = video_mem;
+	  ptr += scr_width*2*r;
+	  ptr += 2*c;
+	  ptr -= 2;
+	  while(!(*str == '\0')){
+		  *ptr = *str;
+		  ptr++;
+		  *ptr = attr;
+		  ptr++;
+		  str++;
+	  }
+	  if(ptr>(ptr_ori+scr_width*2*scr_lines)) return -1;
+	  return 0;
 }
 
 int vt_print_int(int num, char attr, int r, int c) {
-
-  /* To complete ... */
-
+	char *ptr,*ptr_ori;
+	int sinal = 0;
+	ptr = video_mem + scr_width*2*scr_lines;
+	ptr_ori = video_mem;
+	ptr -= scr_width*2*scr_lines- scr_width*2*r;
+	ptr -= scr_width*2-2*c;
+	int i=0,num_cont =num;
+	if(num < 0){
+		num = num*(-1);
+		num_cont = num_cont*(-1);
+		sinal = 1;
+	}
+	while(num_cont>0){
+		num_cont = num_cont / 10;
+		i++;
+	}
+	ptr +=i*2;
+	while(num > 0)
+	{
+		int cenas = num % 10;
+		num = num / 10;
+		ptr--;
+		*ptr = attr;
+		ptr--;
+		*ptr = cenas+0x30;
+	}
+	if (sinal == 1){
+		ptr--;
+		*ptr = attr;
+		ptr--;
+		*ptr = '-';
+	}
+	if(ptr<ptr_ori) return -1;
+	return 0;
 }
 
 
 int vt_draw_frame(int width, int height, char attr, int r, int c) {
-
-  /* To complete ... */
-
+	 char *ptr,*ptr_ori;
+	 ptr = video_mem;
+	 ptr_ori = video_mem;
+	 if(width+c>scr_width||height+r>scr_lines) return -1;
+	 ptr += scr_width*2*r;
+	 ptr += 2*c;
+	 ptr -= 1;
+	 int i,diferenca=scr_width-(width+c);
+	 for(i=0;i<=width;i++){
+		 *ptr = attr;
+		 ptr += 2;
+	 }
+	 ptr -= 2;
+	 for(i=0;i<(height-2);i++){
+		ptr+=diferenca*2;
+		ptr+=c*2;
+		*ptr = attr;
+		ptr += width*2;
+		*ptr = attr;
+	 }
+	 ptr+=diferenca*2 + c*2;
+	 for(i=0;i<=width;i++){
+	 		 *ptr = attr;
+	 		 ptr += 2;
+	 	 }
+	 return 0;
 }
 
 /*
