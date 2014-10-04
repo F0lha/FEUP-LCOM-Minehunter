20088cff6a7e6113072cbf6248267800951333f4
up201305337 <up201305337@d9c0541b-6fd0-4286-b0aa-8419d0fe61cc>
2014-10-04 17:21:33 +0000

    lab2 start

--- lab2/timer.c
+++ lab2/timer.c
@@ -1,6 +1,10 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
 
+int main(){
+
+}
+
 int timer_set_square(unsigned long timer, unsigned long freq) {
 
 	return 1;
@@ -27,6 +31,43 @@ int timer_get_conf(unsigned long timer, unsigned char *st) {
 
 int timer_display_conf(unsigned char conf) {
 	
+	int bcd,prog_mode,type_access,null_counter,output;
+	int temp_conf;
+	temp_conf=conf;
+	temp_conf &= 0x01;
+	bcd=temp_conf;
+	temp_conf = conf;
+	temp_conf >> 1;
+	temp_conf &= 0x111;
+	prog_mode=temp_conf;
+	temp_conf = conf;
+	temp_conf >> 4;
+	temp_conf &= 0x11;
+	prog_mode=temp_conf;
+	temp_conf = conf;
+	temp_conf >> 6;
+	temp_conf &= 0x01;
+	null_counter=temp_conf;
+	temp_conf = conf;
+	temp_conf >> 7;
+	temp_conf &= 0x01;
+	output=temp_conf;
+
+	if(bcd == 1){
+		printf("bcd\n");
+	}
+	else{
+		printf("binary\n");
+	}
+
+	if(null_counter == 1){
+			printf("null_counter = 1\n");
+		}
+		else{
+			printf("null_counter = 0\n");
+		}
+
+
 	return 1;
 }
 
