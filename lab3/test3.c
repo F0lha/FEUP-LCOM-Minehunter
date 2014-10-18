b2b9d07af0cf6ace5a7fc40264530a4a740e7b4b
up201207835 <up201207835@d9c0541b-6fd0-4286-b0aa-8419d0fe61cc>
2014-10-18 18:42:10 +0000

    Subscribe e Unsubscribe done

--- lab3/test3.c
+++ lab3/test3.c
@@ -1,6 +1,63 @@
+#define OUT_BUF 0x60
+unsigned int kbd_hook_id = 0;
+
+
+int kbd_subscribe_int(void) {
+	int value = kbd_hook_id;
+	sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &kbd_hook_id);
+	sys_irqenable(&kbd_hook_id);
+	return value;
+}
+
+int kbd_unsubscribe_int() {
+	return (sys_irqrmpolicy(&kbd_hook_id)||sys_irqdisable(&kbd_hook_id));
+}
+
+void kbd_int_handler(){
+
+}
+
 int kbd_test_scan(unsigned short ass); {
 	/* To be completed */
+	int ipc_status, i = 0, r;
+	message msg;
+
+		kbd_subscribe_int();
+
+		int irq_set = BIT(kbd_subscribe_int());
+
+		while( i!= 0x81 ) {
+			/* Get a request message. */
+			if ( (r=driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+				printf("driver_receive failed with: %d", r);
+				continue;
+			}
+
+			if (is_ipc_notify(ipc_status)) { // received notification
+				switch (_ENDPOINT_P(msg.m_source)) {
+				case HARDWARE: /* hardware interrupt notification */
+					if (msg.NOTIFY_ARG & irq_set) { // subscribed interrupt
+
+						/* process it */
+						if (ass==0){ //C code
+
+						}
+						else { //Assembly code
+
+						}
+					}
+				}
+				break;
+default:
+	break; // no other notifications expected: do nothing
+			}
+
+
+
+	kbd_unsubscribe_int();
 }
+
+
 int kbd_test_leds(unsigned short n, unsigned short *leds) {
     /* To be completed */
 }
