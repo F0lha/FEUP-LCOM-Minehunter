a3317a7b6df63b533d5074542e5b0f425d70e02e
Pedro Castro <F0lha@users.noreply.github.com>
2014-10-20 16:30:23 +0000

    opa

--- lab3/test3.c
+++ lab3/test3.c
@@ -2,71 +2,59 @@
 #include <minix/drivers.h>
 
 #include "test3.h"
-
-#define OUT_BUF 0x60
-unsigned int kbd_hook_id = 0;
-
-
-int main (){
-	sef_startup();
-
-	return 0;
-
-}
+#include "keyboard.h"
+#include "i8254.h"
 
 
-int kbd_subscribe_int(void) {
-	int value = kbd_hook_id;
-	sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &kbd_hook_id);
-	sys_irqenable(&kbd_hook_id);
-	return value;
-}
 
-int kbd_unsubscribe_int() {
-	return (sys_irqrmpolicy(&kbd_hook_id)||sys_irqdisable(&kbd_hook_id));
-}
 
-void kbd_int_handler(){
+int kbd_test_scan(unsigned short ass) {
 
-}
-
-int kbd_test_scan(unsigned short ass); {
-	/* To be completed */
-	int ipc_status, i = 1, r;
+	int two_bytes = 0;
+	int ipc_status, r;
 	message msg;
 
-		kbd_subscribe_int();
+	int irq_set = kbd_subscribe_int();
 
-		int irq_set = BIT(kbd_subscribe_int());
+	while(scan_code!=BREAK_CODE_ESC) {
 
-		while(i) {
-			/* Get a request message. */
-			if ( (r=driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
-				printf("driver_receive failed with: %d", r);
-				continue;
-			}
-
-			if (is_ipc_notify(ipc_status)) { // received notification
-				switch (_ENDPOINT_P(msg.m_source)) {
-				case HARDWARE: /* hardware interrupt notification */
-					if (msg.NOTIFY_ARG & irq_set) { // subscribed interrupt
-
-						/* process it */
-						if (ass==0){ //C code
-							// kbd_int_handler();
+		/* Get a request message. */
+		if ( (r=driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) { // received notification
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /* hardware interrupt notification */
+				if (msg.NOTIFY_ARG & irq_set) {
+					if (ass==0){ //C code
+						kbd_int_handler();
+						if(scan_code==TWO_BYTE_CODE){two_bytes = 1;}
+						else if(scan_code>>7){
+							if(two_bytes) printf("BREAKCODE : 0xE0%x\n",scan_code & 0xff);
+							else printf("BREAKCODE : 0x%x\n",scan_code & 0xff);
+							if (scan_code==BREAK_CODE_ESC)
+								break;
 						}
-						else { //Assembly code
-
+						else
+						{
+							if (two_bytes){
+								printf("MAKCODE : 0xE0%x\n",scan_code & 0xff);
+								two_bytes=0;
+							}
+							else printf("MAKECODE : 0x%x\n",scan_code & 0xff);
 						}
+					}
+					else { //Assembly code
+
 					}
 				}
 				break;
-default:
-	break; // no other notifications expected: do nothing
+			default:
+				break; // no other notifications expected: do nothing
 			}
-
-
-
+		}
+	}
 	kbd_unsubscribe_int();
 }
 
