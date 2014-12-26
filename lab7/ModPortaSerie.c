6ee6936eeaf8f54d7def8e7bbc3124c7050b124a
up201305337 <up201305337@d9c0541b-6fd0-4286-b0aa-8419d0fe61cc>
2014-12-26 14:33:22 +0000

    teste_porta

--- lab7/ModPortaSerie.c
+++ lab7/ModPortaSerie.c
@@ -1,1 +1,122 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+
+
 #include "ModPortaSerie.h"
+
+
+void changeDLAB(unsigned short base_addr,int bit){
+
+	unsigned long LCRbits;
+
+	sys_inb(base_addr + LCR, &LCRbits);
+
+	if(bit == 1) LCRbits = LCRbits|BIT(7);
+	else LCRbits = LCRbits & (~BIT(7));
+
+	sys_outb(base_addr + LCR, LCRbits);
+}
+
+
+void setPorta(unsigned short base_addr, unsigned long bits, unsigned long stop, long parity, unsigned long rate){
+
+
+	unsigned long line_control;
+
+
+	line_control |=  (~BIT(0));//por a 0
+	line_control &=  (~BIT(1));//por a 0
+	if (bits == 6 || bits == 8){
+		line_control |=  (BIT(0));//por a 1
+	}
+	if (bits == 7 || bits == 8){
+		line_control |=  (BIT(1));//por a 1
+	}
+
+
+	line_control &= (~BIT(2)); //por a 0
+	if (stop == 2)
+		line_control |= (BIT(2)); //por a 1
+
+
+
+	if (parity == -1) {
+		line_control &= (~BIT(3));//por a 0
+	} else {
+		line_control |= (BIT(3)); //por a 1
+
+		line_control &= (~BIT(5));//por a  0 porque parity so pode ser none / odd / even
+
+
+		if (parity == 0)
+			line_control |=(BIT(4));//por a 1
+
+		else if (parity == 1)
+			line_control &= (~BIT(4));//por a 0
+	}
+
+	sys_outb(base_addr + LCR, line_control);
+
+
+
+	unsigned long Divisor_Latch = 0;
+	Divisor_Latch = UART_FREQ / rate;
+
+	changeDLAB(base_addr, 1);
+	sys_outb(base_addr + DLM, (Divisor_Latch >> 8));
+
+	sys_outb(base_addr + DLL, Divisor_Latch);
+	changeDLAB(base_addr, 0);
+
+}
+
+
+int getChar(unsigned short base_addr, unsigned char *c) {
+	unsigned long temp1, temp2;
+
+	while (1) {
+
+		sys_inb(base_addr + LSR, &temp1);
+
+		if (temp1 & BIT(RR)) {
+			sys_inb(base_addr + RB, &temp2);
+			*c = temp2;
+			return 0;
+		}
+
+		tickdelay(micros_to_ticks(500));
+	}
+}
+
+int getCharOne(unsigned short base_addr, unsigned char *c) {
+	unsigned long temp1,temp2;
+
+	sys_inb(base_addr + LSR, &temp1);
+
+	if (temp1 & BIT(RR)) {
+		sys_inb(base_addr + RB, &temp2);
+		*c = temp2;
+		return 0;
+	}else return -1;
+
+
+}
+
+int sendChar(unsigned short base_addr, char c) {
+	unsigned long temp1;
+
+
+	while(1) {
+
+		sys_inb(base_addr+LSR, &temp1);
+
+		if (temp1 & BIT(THRE)) {
+			sys_outb(base_addr + TH, c);
+			return 0;
+		}
+
+		tickdelay(micros_to_ticks(2000));
+
+	}
+	return -1;
+}
--- lab7/test7.c
+++ lab7/test7.c
@@ -1,16 +1,122 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+
+#include "test7.h"
+#include "ModPortaSerie.h"
+
+
 int ser_test_conf(unsigned short base_addr) {
-    /* To be completed */
+
+
+	unsigned long Divisor_Latch, dll = 0,dlm = 0;
+	changeDLAB(base_addr, 1);
+
+	sys_inb(base_addr + DLL, &dll);
+	Divisor_Latch |= (dll & 0xFF);
+	sys_inb(base_addr + DLM, &dlm);
+	Divisor_Latch = (dlm & 0xFF) << 8;
+
+	changeDLAB(base_addr, 0);
+	unsigned long bit_rate = UART_FREQ/Divisor_Latch;
+
+
+	unsigned long line_control = 0;
+
+	sys_inb(base_addr + LCR, &line_control);//// LineControl
+
+
+	unsigned long Interrupt_Enable;
+	sys_inb(base_addr + IER, &Interrupt_Enable);
+
+
+	printf("Bit-rate: %d\n", bit_rate);
+
+	///LCR
+	printf("Word length: ");
+	int num = 5;
+	if (BIT(0) & line_control) num += 1;
+	if (BIT(1) & line_control) num += 2;
+	printf("%d\n", num);
+
+	if((line_control & BIT(2)) == 0)
+	printf("N stop bits: %d\n", 1);
+	else printf("N stop bits: %d\n", 2);
+
+	printf("Parity: ");
+	if ((line_control & BIT(3)) == 0)
+		printf("None\n");
+	else {
+		if ((line_control & BIT(5)) == 0) {
+			if ((line_control & BIT(4)) == 0)
+				printf("Odd\n");
+			else
+				printf("Even\n");
+		} else {
+			if ((line_control & BIT(4)) == 0)
+				printf("1\n");
+			else printf("0\n");
+		}
+	}
+	printf("Set Break Enable : %d\n",line_control & BIT(6));
+	///IE
+
+	if (Interrupt_Enable & BIT(0)) printf("Received Data Interrupt : Enabled\n");
+	else printf("Received Data Interrupt : Disabled\n");
+
+	if (Interrupt_Enable & BIT(1)) printf("Transmitter Empty Interrupt : Enabled\n");
+	else printf("Transmitter Empty Interrupt : Disabled\n");
+
+	if (Interrupt_Enable & BIT(2)) printf("Receiver Line Interrupt : Enabled\n");
+	else printf("Receiver Line Interrupt : Disabled\n");
+
 }
 
 int ser_test_set(unsigned short base_addr, unsigned long bits, unsigned long stop, 
 	           long parity, unsigned long rate) { 
-    /* To be completed */
+	setPorta(base_addr,bits,stop, parity, rate);
 }
 
 int ser_test_poll(unsigned short base_addr, unsigned char tx, unsigned long bits, 
                     unsigned long stop, long parity, unsigned long rate, 
                     int stringc, char *strings[]) {
-    /* To be completed */
+
+	ser_test_set(base_addr,bits,stop,parity,rate);
+
+	if(tx == 0)  ///receiver
+	{
+		while(1)
+		{
+			char temp;
+			getChar(base_addr,&temp);
+			if(temp == ' ')
+			{
+				printf("\n");
+			}
+			else if(temp == '.')
+			{
+				printf("\n");
+				break;
+			}
+			else{
+				printf("%c",temp);
+			}
+		}
+	}
+	else{ /// sender
+		int n = 0;
+		while(n != 1)
+		{
+			if(**(strings) == '.')
+			{
+				n = 1;
+			}
+			sendChar(base_addr,*strings);
+			strings++;
+
+		}
+	}
+
+
 }
 
 int ser_test_int(/* details to be provided */) { 
